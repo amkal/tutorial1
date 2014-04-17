8ffe5cd97e42016f7c17974bb187ef4df74eb43f
amkal <amir_k@gmx.at>
2014-04-17 02:45:06 +0200

    Try to use bootstrap

--- src/main/java/com/amirkaljikovicphotography/myheadshots/tutorial/components/Layout.java
+++ src/main/java/com/amirkaljikovicphotography/myheadshots/tutorial/components/Layout.java
@@ -1,15 +1,41 @@
 package com.amirkaljikovicphotography.myheadshots.tutorial.components;
 
-import org.apache.tapestry5.*;
-import org.apache.tapestry5.annotations.*;
-import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.ioc.annotations.Symbol;
 
 /**
  * Layout component for pages of application tutorial1.
  */
-@Import(stylesheet = "context:layout/layout.css")
+@Import(stylesheet={
+        "context:layout/css/bootstrap.css",
+        "context:layout/css/bootstrap-responsive.css"
+        },
+library={
+		"context:layout/js/main.js",
+        "context:layout/js/bootstrap.js",
+        "context:layout/js/html5shiv.js", 
+		"context:layout/js/respond.min.js",
+		"context:layout/js/lib/pace.min.js",
+		"context:layout/js/lib/jquery.min.js",
+		"context:layout/js/bootstrap.min.js",
+		"context:layout/js/lib/smoothscroll.js",
+		"context:layout/js/lib/smoothscroll.js",
+		"context:layout/js/lib/jquery.sudoslider.min.js",
+		"context:layout/js/lib/jquery.parallax.js",
+		"context:layout/js/lib/jquery.magnific-popup.min.js",
+		"context:layout/js/lib/gmap3.min.js",
+		"context:layout/js/lib/jquery.inview.min.js",
+		"context:layout/js/lib/jquery.simple-text-rotator.min.js",
+		"context:layout/js/lib/jquery.mixitup.min.js",
+        }
+)
 public class Layout
 {
     /**
--- src/main/java/com/amirkaljikovicphotography/myheadshots/tutorial/pages/Index.java
+++ src/main/java/com/amirkaljikovicphotography/myheadshots/tutorial/pages/Index.java
@@ -1,50 +1,14 @@
 package com.amirkaljikovicphotography.myheadshots.tutorial.pages;
 
-import java.util.Date;
-import org.apache.tapestry5.annotations.*;
-import org.apache.tapestry5.ioc.annotations.*;
-import org.apache.tapestry5.corelib.components.*;
-import org.apache.tapestry5.SymbolConstants;
-import org.apache.tapestry5.alerts.AlertManager;
+import org.apache.tapestry5.annotations.Import;
 
 /**
  * Start page of application tutorial1.
  */
-public class Index
-{
-    @Property
-    @Inject
-    @Symbol(SymbolConstants.TAPESTRY_VERSION)
-    private String tapestryVersion;
 
-    @InjectComponent
-    private Zone zone;
+@Import(stylesheet = { 
+		"context:layout/css/bootstrap.min.css",
+		"context:layout/css/scheme/red.css" })
 
-    @Persist
-    @Property
-    private int clickCount;
-
-    @Inject
-    private AlertManager alertManager;
-
-    public Date getCurrentTime()
-    {
-        return new Date();
-    }
-
-    void onActionFromIncrement()
-    {
-        alertManager.info("Increment clicked");
-
-        clickCount++;
-    }
-
-    Object onActionFromIncrementAjax()
-    {
-        clickCount++;
-
-        alertManager.info("Increment (via Ajax) clicked");
-
-        return zone;
-    }
+public class Index {
 }
